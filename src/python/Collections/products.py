7283eca85da276a89d5d86c86c65085aff6b4619
sonuhawk <sonu3052@gmail.com>
2025-03-26 09:56:27 +0000

    feat: all final code

--- src/python/Collections/products.py
+++ src/python/Collections/products.py
@@ -3,5 +3,15 @@ import SearchComponents.embeddings as Embeddings
 
 # Function to generate embeddings for product data
 def generateProductEmbedding(product,embeddings_deployment,AzureOpenAIClient):
+     # Construct the product name by concatenating the category name and product name
+    productName = "Category - " + product["categoryName"] + ", Name -" + product["name"]
 
-    pass  # Replace this line with the lab's code
+    # If the product name exists
+    if productName:
+        # Generate embeddings for the product name
+        product["productVector"] = Embeddings.generateEmbeddings (productName,embeddings_deployment,AzureOpenAIClient)
+
+    # Return the product data with the generated embeddings
+    return product
+
+      # Replace this line with the lab's code
